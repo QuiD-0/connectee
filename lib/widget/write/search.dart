02bb3ff8bb500dc0388dc9d8bef68c5fc1ec7ea5
QuiD <wodnd101@naver.com>
2021-08-01 19:59:34 +0900

    recent search

--- lib/widget/write/search.dart
+++ lib/widget/write/search.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Search extends StatefulWidget {
   final type;
@@ -12,11 +14,36 @@ class Search extends StatefulWidget {
 
 class _SearchState extends State<Search> {
   String searchValue;
+  bool visibleRecent;
+  List searchData;
+  List<String> recent = [];
+
+  _getList() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      recent = prefs.getStringList('recent') ?? [];
+    });
+  }
+
+  _saveList(data) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if (data != '') {
+      if (!recent.contains(data)) {
+        if (recent.length == 10) {
+          recent.removeAt(0);
+        }
+        recent.add(data.toString());
+        await prefs.setStringList("recent", recent);
+      }
+    }
+  }
 
   @override
   void initState() {
     // TODO: implement initState
     searchValue = '';
+    visibleRecent = true;
+    _getList();
     super.initState();
   }
 
@@ -55,9 +82,10 @@ class _SearchState extends State<Search> {
                     color: Color(0xff2d2d2d),
                     borderRadius: BorderRadius.circular(13)),
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Container(
+                      padding: EdgeInsets.only(left: 15),
                         width: 300,
                         child: TextField(
                           onChanged: (text) {
@@ -74,17 +102,100 @@ class _SearchState extends State<Search> {
                     IconButton(
                         splashColor: Colors.transparent,
                         onPressed: () {
-                          print("$searchValue");
+                          //검색어 저장
+                          _saveList(searchValue);
+                          print(recent);
+                          //최근 검색어 끄기
+                          setState(() {
+                            visibleRecent = false;
+                          });
+                          //api통신
                         },
                         icon: Icon(
                           Icons.search_rounded,
                           color: Colors.white,
                         )),
-
                   ],
                 ),
               ),
               // 최근 검색어
+              visibleRecent
+                  ? Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 25, 10, 0),
+                      child: Container(
+                        constraints: BoxConstraints(minHeight: 100),
+                        decoration: BoxDecoration(
+                          color: Color(0xff2d2d2d),
+                          borderRadius: BorderRadius.circular(13),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(15),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    '최근 검색어',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Container(
+                                    padding: EdgeInsets.fromLTRB(12, 5, 12, 5),
+                                    decoration: BoxDecoration(
+                                        color: Color(0xff4d4d4d),
+                                        borderRadius:
+                                            BorderRadius.circular(30)),
+                                    child: GestureDetector(
+                                      onTap: () async {
+                                        SharedPreferences prefs =
+                                            await SharedPreferences
+                                                .getInstance();
+                                        prefs.clear();
+                                        _getList();
+                                      },
+                                      child: Text('전체삭제',
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 12)),
+                                    ),
+                                  )
+                                ],
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              recent.isNotEmpty
+                                  ? Wrap(
+                                      alignment: WrapAlignment.start,
+                                      children: recent.reversed
+                                          .map((item) => Container(
+                                              margin: EdgeInsets.all(5),
+                                              padding: EdgeInsets.fromLTRB(
+                                                  13, 5, 13, 5),
+                                              decoration: BoxDecoration(
+                                                  color: Color(0xff9D9D9D),
+                                                  borderRadius:
+                                                      BorderRadius.circular(
+                                                          13)),
+                                              child: Text(item)))
+                                          .toList()
+                                          .cast<Widget>(),
+                                    )
+                                  : Text('최근 검색어가 없습니다.',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                      )),
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                  : Text('asd'),
 
               //검색 결과
             ],
