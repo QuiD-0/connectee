d19e07ba61814d81638917f67f8ff65bc69db083
QuiD <wodnd101@naver.com>
2021-08-31 17:08:36 +0900

    update

--- lib/widget/write/search.dart
+++ lib/widget/write/search.dart
@@ -1,10 +1,10 @@
 import 'dart:convert';
+import 'package:connectee/secret.dart';
 import 'package:html/parser.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:connectee/secret.dart';
 
 class Search extends StatefulWidget {
   final type;
